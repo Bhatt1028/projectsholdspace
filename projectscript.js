fe63c1a459a1e4c0339d983691533853ebeabbbb
Bhatt1028 <43218176+Bhatt1028@users.noreply.github.com>
2018-09-28 14:03:07 -0500

    nower
    orer neverer

--- projectscript.js
+++ projectscript.js
@@ -38,11 +38,10 @@ function tempbug()
 {
 var num1 = Number(document.project2.input1.value);
 
-
-
-
     if(num1 <= 273.15)
 {
+    document.getElementById('spring').remove();
+    document.getElementById('summer').remove();
     document.getElementById('fall').remove();
     return document.getElementById('winter').src = "perimg/winter.jpg";
 }
@@ -60,6 +59,110 @@ else if (num1 >= 299.817)
 }
 
 
-
 }
 // thanks to w3 schools for how to remove pictures
+
+function calcount()
+{
+var cod = document.project3.input1.value.toLowerCase();
+var num1 = Number(document.project3.input2.value);
+
+if (cod == 'wl')
+{
+    return 2 * num1
+}
+if (cod == 'tv')
+{
+    return 0.1 * num1
+}
+if (cod == 'rn')
+{
+    return 3 * num1
+}
+if (cod == 'lw')
+{
+    return 10 * num1
+}
+if (cod == 'vg')
+{
+    return 1.1 * num1
+}
+if (cod == 'ef')
+{
+    return 123 * num1
+}
+
+
+}
+
+
+/*
+function pluralize()
+{
+var quantity = Number(document.project4.num1.value);   //get quantity
+    var noun = document.project4.word1.value.toLowerCase();
+
+   if (noun == 'mouse') // special if-else for case of mouse
+   {
+       if(quantity > 1)
+       {
+           return quantity + " mice";
+       }
+       else
+       {
+           return quantity + " mouse";
+       }
+   }
+
+    if (noun == 'deer') // special if-else for case of deer
+    {
+        return quantity + " deer";
+    }
+
+    if (noun == 'cactus') // special if-else for case of cacti
+    {
+        if(quantity > 1)
+        {
+            return quantity + " cacti";
+        }
+        else
+        {
+            return quantity + " cactus";
+        }
+    }
+
+    if (quantity == 1)
+    {
+        return quantity + " " + noun;
+    }
+    else if (quantity >= 1)
+    {
+        return quantity + " " +  noun + "s";
+    }
+    else if (quantity < 1)
+    {
+        return quantity + " " + noun + "s";
+    }
+
+
+}
+
+
+ */
+/*
+<form name="project4">
+    <h4>Word Pluralinator</h4>
+    <br>
+    Type a quantity:
+    <input name="num1" type="text">
+    <br><br>
+    Type in noun:
+    <input name="word1" type="text">
+    <br><br>
+
+    Result: <input name="result" type="text" readonly="true" style="border-style: none">
+    <br><br>
+    <button type="button" onclick="document.project4.result.value=pluralize()">Plural it!</button>
+
+</form>
+ */
